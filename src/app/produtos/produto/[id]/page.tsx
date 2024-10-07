70029373cb721521fa9eae01c2c23519b0a95f68
Marsol4 <enzogmarsola@gmail.com>
2024-10-07 09:41:00 -0300

    Criando o projeto FIAP-Eletro

---
+++ src/app/produtos/produto/[id]/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+import { TipoProduto } from "@/types"
+import { useEffect, useState } from "react"
+
+export default function produto({params}:{params:{id:number}}){
+
+    const [produto, setProduto] = useState<TipoProduto>()
+
+    useEffect(()=>{
+        const chamadaApi = async ()=>{
+            const response = await fetch(`http://localhost:3000/api/base-produtos/${params.id}`)
+            const data = await response.json()
+            setProduto(data)
+            console.log(data);
+            
+        }
+        chamadaApi()
+    },[]) /*[]-> indica que só ocorrerá apos a pagina ser carregada*/
+
+    return(
+        <main>
+            <h2>Produto</h2>
+
+            <div>
+                <p>Id: {produto?.id} {/*Coloca o ? pois há a possibilidade desse valor não receber nada*/}</p>
+                <p>Nome: {produto?.nome}</p>
+                <p>Preço: {produto?.preco}</p>
+                <p>Estoque: {produto?.estoque}</p>
+            </div>
+        </main>
+    )
+}
